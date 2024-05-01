479e52bcaf825a75ed8e20129d78b9a650c9a77c
Xfreak22 <139982849+Xfreak22@users.noreply.github.com>
2024-05-01 12:34:34 +0530

    Converted Add subscriber Class based component to functional based component
    Converted Add subscriber Class based component to functional based component.

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,67 +1,62 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import './common/common1.css';
 import Header from "./Header";
 import Button1 from "./button";
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
-class AddSubscriber extends Component {
-
+export default function AddSubscriber(props) {
+   
+    const[addSubscriberForm,setaddSubscriberForm] = useState({
+        id:0,
+        name:'',
+        phone:''
+    })
    
 
-    constructor (){
-        super();
-        this.state ={
-            id:0,
-            name:'',
-            phone:''
-        }
-    }
+    const navigate = useNavigate();
 
-        inputChangedHandler = (e) =>{
-            const state = this.state;
-            state[e.target.name] = e.target.value;
-            this.setState(state);
-        }
-    
-        onFormSubmitted = (e) =>{
-        e.preventDefault();
-        this.props.addsubscriberHandler(this.state);
-        this.setState({id:0, name:'', phone:''});
-        
-        }
-        
-    render(){   
-        const {name,phone} = this.state;
-        return(
-            <Fragment>
-        <div className="header-styling">
-        <Header heading="Add Subsrciber"/>
+
+               function inputChangedHandler (e) {
+                    const state = addSubscriberForm;
+                    state[e.target.name] = e.target.value;
+                    setaddSubscriberForm({...state});
+                }
+            
+                const onFormSubmitted = (e) =>{
+                e.preventDefault();
+                props.addsubscriberHandler(addSubscriberForm);
+                setaddSubscriberForm({id:0, name:'', phone:''});
+                navigate("/");
+                }
+
+    const {name,phone} = addSubscriberForm;
+
+    return(
+        <Fragment>
+    <div className="header-styling">
+    <Header heading="Add Subsrciber"/>
+    </div>
+    <div>
+    <Link to="/"><Button1 heading="Back"/></Link>
+    </div>
+    <div>
+        <form className="form-style" onSubmit={onFormSubmitted}>
+         <label className="label-design" htmlFor="name">Name: </label><br/>
+         <input className="input-design" id="name" name="name" onChange={inputChangedHandler}/><br/>
+         <label className="label-design" htmlFor="phone">Phone: </label><br/>
+         <input className="input-design" id="phone" name="phone" onChange={inputChangedHandler}/><br/>
+         
+        <div className="span-container-for-subscriber">
+         <span>Subsrciber to be added: </span><br/>
+         <span className="label-design">Name: {name}</span><br/>
+         <span className="label-design">Phone: {phone}</span><br/> 
+         <button type="submit" className="button-style2">Add</button>
         </div>
-        <div>
-        <Link to="/"><Button1 heading="Back"/></Link>
-        </div>
-        <div>
-            <form className="form-style" onSubmit={this.onFormSubmitted.bind(this)}>
-             <label className="label-design" htmlFor="name">Name: </label><br/>
-             <input className="input-design" id="name" name="name" onChange={this.inputChangedHandler}/><br/>
-             <label className="label-design" htmlFor="phone">Phone: </label><br/>
-             <input className="input-design" id="phone" name="phone" onChange={this.inputChangedHandler}/><br/>
-             
-            <div className="span-container-for-subscriber">
-             <span>Subsrciber to be added: </span><br/>
-             <span className="label-design">Name: {name}</span><br/>
-             <span className="label-design">Phone: {phone}</span><br/> 
-             <button type="submit" className="button-style2">Add</button>
-            </div>
-            </form>
-        </div>
-        </Fragment>
-        )
-    }
+        </form>
+    </div>
+    </Fragment>
+    )
 }
-
-
-export default AddSubscriber;
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,44 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Header from './Header';
 import './ShowSubscribers.css';
 import './common/common1.css';
 import Button1 from './button';
 import { Link } from 'react-router-dom';
 
+export default function ShowSubscribers({SubscriberList,deletesubscriberHandler}) {
 
-class ShowSubscribers extends Component {
- 
-  onDeletedClick = (subscriberId) =>{
-    this.props.deletesubscriberHandler(subscriberId);
-  }
+  return (
+    <div >
+      <div className="header-styling">
+      <Header heading="Phone Directory"/>
+      </div>
+     <Link to="/add"><Button1 heading="Add"/></Link>
+  <div className='sub-heading-div-margin-for-name-and-num'>
+   <span style={{color: "darkslategrey", margin: 30, padding: 10, width: "auto"}}>Name</span>
+   <span style={{color: "darkslategrey", margin: "auto", padding: 10, width: "auto"}}>Phone</span>
+  </div>
 
+    { 
+    SubscriberList.map(sub =>{
+       return  <div key={sub.id} className='Content'>
+              <span className='Content-span-styling'> {sub.name}</span>   
+              <span className='Content-span-styling1'>{sub.phone}</span>
+              <button type='submit' className='button-style3' onClick={()=>deletesubscriberHandler(sub.id)}>Delete</button>
+               </div>
+      })
+    } 
 
-
-  render() {
-    return (
-      <div >
-        <div className="header-styling">
-        <Header heading="Phone Directory"/>
-        </div>
-       <Link to="/add"><Button1 heading="Add"/></Link>
-    <div className='sub-heading-div-margin-for-name-and-num'>
-     <span style={{color: "darkslategrey", margin: 30, padding: 10, width: "auto"}}>Name</span>
-     <span style={{color: "darkslategrey", margin: "auto", padding: 10, width: "auto"}}>Phone</span>
-    </div>
- 
-      { 
-        this.props.SubscriberList.map(sub =>{
-         return  <div key={sub.id} className='Content'>
-                <span className='Content-span-styling'> {sub.name}</span>   
-                <span className='Content-span-styling1'>{sub.phone}</span>
-                <button type='submit' className='button-style3' onClick={this.onDeletedClick.bind(this,sub.id)}>Delete</button>
-                 </div>
-        })
-      } 
-
-    </div>
-    )
-  }
+  </div>
+  )
 }
 
-export default ShowSubscribers;
